eabab2792feaaff8a2069bb0a5f839bcb7572509
Morten Slott Hansen <msh@vivino.com>
2026-05-29 23:13:13 +0200

    Release 1.1.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "shared",
-            url: "https://github.com/Vivino/vivino-shared-spm-v2/releases/download/1.0.3/sharedKit.xcframework.zip",
+            url: "https://github.com/Vivino/vivino-shared-spm-v2/releases/download/1.1.0/sharedKit.xcframework.zip",
             checksum: "c1138001c2b92831c79e5673204c699f3febbdf9bd92a3ae5c64e31be0e43dd1"
         ),
     ]
