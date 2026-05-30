762526c5835f1141e4e1044c344432931b352b2c
Morten Slott Hansen <msh@vivino.com>
2026-05-30 11:08:06 +0200

    Fix UI issues and update 1.2.3

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "shared",
-            url: "https://github.com/Vivino/vivino-shared-spm-v2/releases/download/1.1.11/sharedKit.xcframework.zip",
+            url: "https://github.com/Vivino/vivino-shared-spm-v2/releases/download/1.2.3/sharedKit.xcframework.zip",
             checksum: "d66a6d16436cab8eca58a326440de09d72b2e9dff07f115d11c0c59798cb7696"
         ),
     ]
